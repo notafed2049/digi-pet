1aa391fb923c3e7a54365059eac97f933ea51310
Akmal <izuddin0407@gmail.com>
2022-12-13 20:50:26 +0800

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  // fonts: {
+  //   heading: `'Alfa Slab One', alfa-slab-one`,
+  //   body: `'Alfa Slab One', alfa-slab-one`,
+  // },
+  styles: {
+    global: {
+      'html, body': {
+        backgroundColor: 'red.800',
+        color: 'black'
+      },
+    }
+  }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <ChakraProvider theme={ theme }>
+      <Component { ...pageProps } />
+    </ChakraProvider>
+  );
+};
 
 export default MyApp
