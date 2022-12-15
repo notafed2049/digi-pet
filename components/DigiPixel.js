01ba968661db948e5c6ebbde4bd300a907f61048
Akmal <izuddin0407@gmail.com>
2022-12-15 21:26:04 +0800

    minor additions => added the mirroring effect animation

--- components/DigiPixel.js
+++ components/DigiPixel.js
@@ -2,12 +2,31 @@ import React from 'react';
 
 import { 
   Image,
+  keyframes
  } from '@chakra-ui/react';
+
+import { motion } from 'framer-motion';
 
 export const DigiPixel = ({ digimon }) => {
 
+  const animationKeyframes = keyframes`
+    0% {
+      transform: scaleX(1);
+    }
+    50% {
+      transform: scaleX(-1);
+    }
+    100% {
+      transform: scaleX(1);
+    }
+  `;
+
+  const animation = `${animationKeyframes} 10s steps(1) infinite`;
+
   return (
     <Image
+      as={ motion.img }
+      animation={ animation }
       src={ digimon.sprite }
       alt={ digimon.name }
     />
